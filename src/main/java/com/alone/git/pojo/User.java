0f7c073782a844717a962258a71575a00323376f
alone_xuxu <1547756164@qq.com>
2017-10-11 16:59:31 +0800

    asdfasdfasdf

---
+++ src/main/java/com/alone/git/pojo/User.java
@@ -0,0 +1,106 @@
+/**
+ * @author alone_xuxu
+ * @createTime 2017年10月11日
+ */
+package com.alone.git.pojo;
+
+/**
+ * @author alone_xuxu
+ *
+ * @createTime 2017年10月11日
+ *
+ */
+public class User {
+	
+	
+	private String name;
+	private String nick;
+	private Integer age;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the nick
+	 */
+	public String getNick() {
+		return nick;
+	}
+	/**
+	 * @param nick the nick to set
+	 */
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	/**
+	 * @return the age
+	 */
+	public Integer getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", nick=" + nick + ", age=" + age + "]";
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((nick == null) ? 0 : nick.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (nick == null) {
+			if (other.nick != null)
+				return false;
+		} else if (!nick.equals(other.nick))
+			return false;
+		return true;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/alone/git/pojo/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author alone_xuxu
+ * @createTime 2017年10月11日
+ */
+/**
+ * @author alone_xuxu
+ *
+ * @createTime 2017年10月11日
+ *
+ */
+package com.alone.git.pojo;
